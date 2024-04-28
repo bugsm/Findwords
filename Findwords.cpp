eeec61ede6dbeb116b008a0e891006acba00932a
bugsm <121507446+bugsm@users.noreply.github.com>
2024-04-28 14:45:25 +0700

    Update Findwords.cpp
    perbaiki bug pada kode

--- Findwords.cpp
+++ Findwords.cpp
@@ -27,7 +27,8 @@ void bacadata(const string& namaFile, char data[ROW][COL]) {
 
     file.close();
 }
-bool searchWord(char grid[ROW][COL], string word) {
+
+bool searchWord(char data[ROW][COL], string word) {
     int len = word.length();
 
     if (len > ROW && len > COL) {
@@ -36,14 +37,14 @@ bool searchWord(char grid[ROW][COL], string word) {
 
     for (int row = 0; row < ROW; row++) {
         for (int col = 0; col < COL; col++) {
-            if (tolower(grid[row][col]) == tolower(word[0])) {
+            if (tolower(data[row][col]) == tolower(word[0])) {
                 for (int x = -1; x <= 1; x++) {
                     for (int y = -1; y <= 1; y++) {
                         int k, rd = row + x, cd = col + y;
                         for (k = 1; k < len; k++) {
                             if (rd >= ROW || rd < 0 || cd >= COL || cd < 0)
                                 break;
-                            if (tolower(grid[rd][cd]) != tolower(word[k]))
+                            if (tolower(data[rd][cd]) != tolower(word[k]))
                                 break;
                             rd += x, cd += y;
                         }
@@ -61,6 +62,7 @@ int main() {
    string namaFile = "file.txt"; 
     char data[ROW][COL];
     bacadata(namaFile, data);
+
     int n;
     cin >> n;
     vector<string> words(n);
@@ -68,13 +70,12 @@ int main() {
         cin >> words[i];
     }
     
-
     for (const auto &word : words) {
-        if (searchWord(grid, word))
-            cout <<word << " ada \n";
+        if (searchWord(data, word))
+            cout <<word << " Ada \n";
         else
-            cout<< word << " tidak ada\n";
+            cout<< word << " Tidak Ada\n";
     }
 
-return 0;
+    return 0;
 }
